eb4c0b1689894962ddb7301bb83d966cec241f86
Jack Morris <jxmorris12@gmail.com>
2018-03-25 00:06:54 -0400

    connect dialogueBot api

--- jackjamesjt/MessageController.swift
+++ jackjamesjt/MessageController.swift
@@ -7,23 +7,54 @@
 //
 
 import UIKit
+import ApiAI
+import AVFoundation
 
 class MessageController: UIViewController {
 
     @IBOutlet weak var ChatBox: UITextField!
     
     @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
-    //KEYBOARD AND TEXT FIELD SET UP
     
+    let speechSynthesizer = AVSpeechSynthesizer()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
         NotificationCenter.default.addObserver(self, selector: #selector(MessageController.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
         
         NotificationCenter.default.addObserver(self, selector: #selector(MessageController.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
         
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func speechAndText(text: String) {
+        let speechUtterance = AVSpeechUtterance(string: text)
+        speechSynthesizer.speak(speechUtterance)
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: .curveEaseInOut, animations: {
+            print("text:", text)
+        }, completion: nil)
+    }
+    
+    @IBAction func sendButtonPressed(_ sender: Any) {
+        let request = ApiAI.shared().textRequest()
         
+        if let text = self.ChatBox.text, text != "" {
+            request?.query = text
+        } else {
+            return
+        }
+        
+        request?.setMappedCompletionBlockSuccess({ (request, response) in
+            let response = response as! AIResponse
+            if let textResponse = response.result.fulfillment.speech {
+                self.speechAndText(text: textResponse)
+            }
+        }, failure: { (request, error) in
+            print(error!)
+        })
+        
+        ApiAI.shared().enqueue(request)
+        ChatBox.text = ""
     }
     
     override func viewDidAppear(_ animated: Bool) {
