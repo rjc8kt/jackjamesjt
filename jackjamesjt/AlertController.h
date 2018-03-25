5abe4ba7ffbc3acfb4fc0540c090525f72607715
Richard James Childress <rjameschildress@d-172-25-203-81.dhcp.virginia.edu>
2018-03-24 20:45:28 -0400

    UI for Messaging and Alert

---
+++ jackjamesjt/AlertController.h
@@ -0,0 +1,13 @@
+//
+//  AlertController.h
+//  jackjamesjt
+//
+//  Created by Richard James Childress on 3/24/18.
+//  Copyright © 2018 Richard James Childress. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AlertController : NSObject
+
+@end
